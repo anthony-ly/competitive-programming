37253e24bb8524b9e4c043185c0c8bcfb1d6e6e0
Anthony Ly <director2911@gmail.com>
2021-11-28 18:48:42 +1100

    Solution for N-sum.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt()+scanner.nextInt());
+        int inputNums = scanner.nextInt();
+        int output = 0;
+        for (int i = 0; i < inputNums; i++) {
+            output += scanner.nextInt();
+        }
+        System.out.println(output);
         scanner.close();
     }
 }
