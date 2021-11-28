4911167cd301bf9f8d9e16091fa96074a0ab7bf8
Anthony Ly <director2911@gmail.com>
2021-11-28 23:25:56 +1100

    Solution for Batter Up.

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int hands = scanner.nextInt();
-        String suit = scanner.next();
-        int output = 0;
+        int bats = scanner.nextInt();
+        int sum = 0;
+        int nSum = 0;
 
-        for (int i = 0; i < 4*hands; i++) {
-            String card = scanner.next();
-            
-            if (card.substring(1).equals(suit)) {
-                if (card.substring(0,1).equals("J")) {
-                    output += 20;
-                    continue;  
-                } 
-                if (card.substring(0,1).equals("9")) {
-                    output += 14;
-                    continue;
-                }
+        for (int i = 0; i < bats; i++) {
+            int bases = scanner.nextInt();
+            if (bases == -1) {
+                nSum+=1;
+                continue;
             }
-
-            if (card.substring(0,1).equals("A")) output += 11;
-            if (card.substring(0,1).equals("K")) output += 4;
-            if (card.substring(0,1).equals("Q")) output += 3;
-            if (card.substring(0,1).equals("J")) output += 2;
-            if (card.substring(0,1).equals("T")) output += 10;
+            sum += bases;
         }
-        System.out.println(output);
+
+        System.out.println((double)sum/(bats-nSum));
         scanner.close();
     }
 }
