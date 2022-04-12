8ce5548c0023bd968c4e448e794f525b8b7441fa
anthony-ly <director2911@gmail.com>
2022-04-13 00:07:39 +1000

    Solution for Betting.

---
+++ src/kattis/Betting.java
@@ -0,0 +1,16 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class Betting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int switchPointsA = scanner.nextInt();
+        int switchPointsB = 100 - switchPointsA;
+
+        System.out.println((float) 100 / switchPointsA);
+        System.out.println((float) 100 / switchPointsB);
+
+    }
+
+}
